1863ad872285dba96aede2c2f9ca5120454015ec
svnldwg <s.ludwig.g@gmail.com>
2020-08-02 23:22:55 +0200

    add waypoints, enemy and enemy movement

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0; 
+
+    private void Start() {
+        target = Waypoints.waypoints[0];
+    }
+
+    private void Update() {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f) {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.waypoints.Length - 1) {
+            Destroy(gameObject);
+            return;
+        }
+
+        target = Waypoints.waypoints[++waypointIndex];
+    }
+}
