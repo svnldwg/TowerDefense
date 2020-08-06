9245735181926fb186c89cd2e22ecd4b07f7e1cc
svnldwg <s.ludwig.g@gmail.com>
2020-08-07 00:16:05 +0200

    build turrets

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+    private GameObject turretToBuild;
+
+    public GameObject standardTurretPrefab;
+
+    private void Awake() {
+        if (instance != null) {
+            Debug.LogError("More than one BuildManager in scene");
+            return;
+        }
+        instance = this;
+    }
+
+    private void Start() {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    public GameObject getTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+    private GameObject turret;
+    private Color startColor;
+    private float startGlossiness;
+    private Renderer rend;
+
+    private void Start() {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+        startGlossiness = rend.material.GetFloat("_Glossiness");
+    }
+
+    private void OnMouseEnter() {
+        setMaterialHover();
+    }
+
+    private void OnMouseExit() {
+        resetMaterial();
+    }
+
+    private void OnMouseUpAsButton() {
+        if (turret != null) {
+            Debug.Log("Can't build there! - TODO; Display on screen");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.getTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    private void setMaterialHover()
+    {
+        rend.material.color = hoverColor;
+        rend.material.SetFloat("_Glossiness", 0f);
+    }
+
+    private void resetMaterial()
+    {
+        rend.material.color = startColor;
+        rend.material.SetFloat("Glossiness", startGlossiness);
+    }
+}
